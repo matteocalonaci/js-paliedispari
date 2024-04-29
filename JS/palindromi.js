b6a6b483494a7a2b40d596196d56f17c8d8efdd5
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-04-29 19:06:43 +0200

    commit 2

--- JS/palindromi.js
+++ JS/palindromi.js
@@ -4,3 +4,37 @@
 // - Sono accettate solo soluzioni che fanno uso del ciclo.
 // - Eventuali soluzioni più "comode" potete aggiungere come extra in un commit separato solo DOPO.
 
+
+// - Chiedere all’utente di inserire una parola.
+let parola = prompt("Inserisci una parola");
+console.log(parola, typeof parola);
+
+
+//scomposizione della parola in caratteri
+let caratteri = parola.split('');
+console.log(caratteri);
+
+
+//creo una versione invertita della parola
+let invertita = caratteri.reverse().join('');
+console.log(invertita);
+
+//verifica se la parola è palindroma
+if (parola === invertita) {
+    console.log(`${parola} è una parola palindroma`)
+} else {
+    console.log(`${parola} non è una parola palindroma`)
+
+}
+
+
+//difinizione di funzione
+function palindroma(parola) {
+    for (let i = 0; parola.length / 2; i++)
+        //se un caratterenon corrisponde al carattere dell'altra metà la parola non è PALINDROMA
+        if (parola[i] !== parola[parola.length - i - 1]) {
+            return false;
+        }
+    // Parola è PALINDROMA
+    return true;
+}
--- JS/pariDispari.js
+++ JS/pariDispari.js
@@ -4,3 +4,53 @@
 // - Il computer estrae un numero casuale sempre tra 1 e 5 (usando una funzione).
 // - Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
 // - Dichiariamo chi ha vinto.
+
+
+// chiediamo all'utente su cosa puntare
+let sceltaUtente = prompt("scegli pari o dispari")
+console.log(`Hai scelto: ${sceltaUtente}`);
+
+// chiedo all'utente un numero compreso tra 1 e 5
+let sceltaUtenteNumero = parseInt(prompt("scegli un numero da 1 a 5"));
+console.log(`Hai scelto il numero:${sceltaUtenteNumero}`);
+
+
+// funzione numero random per pc
+let numeroComputer = getRandomNumber(1, 5);
+
+// input: range inteso come min e max 
+// Output: un numero casuale in quel range
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(`La CPU ha scelto:${numeroComputer}`);
+
+// somma risultati
+let risultato = sceltaUtenteNumero + numeroComputer;
+console.log(`La somma è ${risultato}`);
+
+
+function pariOrDispari(risultato) {
+
+    //controllo se valore fornito sia pari o dispari
+    if (risultato % 2 == 0) {
+        let odd = "pari";
+        return odd;
+    }
+
+    return "dispari";
+
+
+}
+
+let odd = "pari"
+let even = "dispari"
+// il vincitore è...
+if (risultato == odd && sceltaUtente == odd) {
+    console.log("HAI VINTO!");
+} else if (risultato == even && sceltaUtente == even) {
+    console.log("HAI VINTO!");
+} else {
+    console.log("HAI PERSO!");
+
+}
